c0d26943f1a36aa3602436504e29a25154984003
NilamK05 <nilam.korade05@gmail.com>
2024-12-14 11:17:00 +0530

    B

--- src/practice/pracClass1.java
+++ src/practice/pracClass1.java
@@ -3,6 +3,13 @@ package practice;
 public class pracClass1 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class1");
+<<<<<<< Updated upstream
 	}
  
+=======
+		System.out.println("Branch B changes");
+		System.out.println("ss");
+	}
+>>>>>>> Stashed changes
 }
+
--- src/practice/pracClass2.java
+++ src/practice/pracClass2.java
@@ -3,5 +3,11 @@ package practice;
 public class pracClass2 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class2");
+<<<<<<< Updated upstream
 	}
+=======
+		System.out.println("ss");
+	}
+
+>>>>>>> Stashed changes
 }
