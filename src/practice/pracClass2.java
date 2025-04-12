d2aa4557ccad26e586a181abcba20fda1a0400f8
NilamK05 <nilam.korade05@gmail.com>
2025-04-12 17:14:36 +0530

    Update pracClass2.java

--- src/practice/pracClass2.java
+++ src/practice/pracClass2.java
@@ -3,14 +3,4 @@ package practice;
 public class pracClass2 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class2");
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-	}
-=======
-=======
->>>>>>> Stashed changes
-		System.out.println("ss");
-	}
-
->>>>>>> Stashed changes
 }
